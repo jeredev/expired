09f821fa151e7c340b3ae0b2162f1d755acfdbdf
Jeremy Wynn <jeremy@jeredev.com>
2022-05-12 16:08:02 -0500

    Renaming of R2 worker

--- src/routes/api/items.ts
+++ src/routes/api/items.ts
@@ -53,8 +53,8 @@ export async function get(event: RequestEvent) {
         // Cloudflare R2 start
         data.forEach((item) => {
           if (item.imagePath) {
-            // item.image = `https://expired-worker.jeredev.workers.dev/${event.locals.user.account.id}/${item.imagePath}.webp`
-            item.image = `https://expired-worker.jeredev.workers.dev/${event.locals.user.account.id}/${item.imagePath}.webp`
+            // item.image = `https://expired-media-worker.jeredev.workers.dev/${event.locals.user.account.id}/${item.imagePath}.webp`
+            item.image = `https://expired-media-worker.jeredev.workers.dev/${event.locals.user.account.id}/${item.imagePath}.webp`
           }
         })
         return {
